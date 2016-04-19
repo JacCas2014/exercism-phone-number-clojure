51d52a761a99d7c4afb0107e20a7a2695b304c5d
Jac Casey <JacCas2014@users.noreply.github.com>
2016-04-19 23:11:27 +0100

    tidying up line spacing and indentation in namespace

--- src/phone_number.clj
+++ src/phone_number.clj
@@ -1,8 +1,8 @@
 (ns phone-number
- (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]))
 
 (defn number-without-punctuation-or-blanks [phone-number]
- (str/replace phone-number #"\p{Blank}|\p{Punct}" ""))
+  (str/replace phone-number #"\p{Blank}|\p{Punct}" ""))
 
 (defn change-to-valid-number [phone-number]
   (cond
@@ -10,7 +10,7 @@
     (and (= (count phone-number) 11) (str/starts-with? phone-number "1")) (str/replace-first phone-number "1" "")
     :else "0000000000"))
 
- (defn number [phone-number]
+(defn number [phone-number]
   (-> phone-number
       number-without-punctuation-or-blanks
       change-to-valid-number))
